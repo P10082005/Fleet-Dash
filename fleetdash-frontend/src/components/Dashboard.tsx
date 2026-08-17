fbfb9bbaa21fd4482e24a50ca326caed1ade4d2d
Sakshi Tewale <sakshitewale1448@gmail.com>
2026-08-17 20:21:13 +0530

    Create React frontend project

---
+++ fleetdash-frontend/src/components/Dashboard.tsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>FleetDash</h1>
+      <p>High-Throughput Event-Driven Fleet Telemetry Dashboard</p>
+    </div>
+  );
+}
+
+export default Dashboard;
