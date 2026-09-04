0d10760bafc2e99ca71f6e9b253983a34fcd760c
Sakshi Tewale <sakshitewale1448@gmail.com>
2026-09-04 22:38:55 +0530

    fix map

--- fleetdash-frontend/src/components/MapView.tsx
+++ fleetdash-frontend/src/components/MapView.tsx
@@ -15,18 +15,24 @@ function MapView({ vehicles }: MapViewProps) {
         style={{ height: "500px", width: "100%" }}
       >
         <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
+          attribution="&copy; OpenStreetMap contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+
         {vehicles.map((vehicle) => (
-          <Marker 
-        key={vehicle.vehicleId}
-        position={[vehicle.latitude, vehicle.longitude]}>
-          <popup>
-            <strong>Vehicle:</strong> {vehicle.vehiclesId}<br />
-            <strong>Speed:</strong> {vehicle.speed} km/h <br />
-            <strong>Status:</strong> {vehicle.status}<br />
-          </popup>
+          <Marker
+            key={vehicle.vehicleId}
+            position={[vehicle.latitude, vehicle.longitude]}
+          >
+            <Popup>
+              <strong>Vehicle:</strong> {vehicle.vehicleId}
+              <br />
+              <strong>Speed:</strong> {vehicle.speed} km/h
+              <br />
+              <strong>Status:</strong> {vehicle.status}
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
